2ee3f70baaa4c4f8d1c14ecd8d65c11acfa25e8b
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-24 09:06:45 -0400

    carpeta settings

---
+++ SistemaDiscusiones/SistemaDiscusiones/settings/base.py
@@ -0,0 +1,51 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'omt6nbk#)yvgry-%z0u)@1@61#c*l2z29!1j9x#%ajb#u^0_9d'
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+	)
+
+#Aplicaciones de terceros
+THIRD_PARTY_APPS = (
+
+	)
+
+#aplicaciones locales
+LOCAL_APPS = (
+
+	)
+
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'SistemaDiscusiones.urls'
+
+WSGI_APPLICATION = 'SistemaDiscusiones.wsgi.application'
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/La_Paz'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True

400301afe04554eb82faeb9cfc786c5059bd9461
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-25 14:16:44 -0400

    templates

--- SistemaDiscusiones/SistemaDiscusiones/urls.py
+++ SistemaDiscusiones/SistemaDiscusiones/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls import url,include,url
 from django.contrib import admin
-
+#Registro de las urls
 urlpatterns = [
 	url(r'^' , include('apps.home.urls')),
     url(r'^admin/', admin.site.urls),
--- SistemaDiscusiones/apps/home/urls.py
+++ SistemaDiscusiones/apps/home/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from .views import IndexView
+#Urls de la aplicacion home
 urlpatterns = [
 	url(r'^$' , IndexView.as_view())
 ]
